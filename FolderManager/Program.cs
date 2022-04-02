eb2f124be153be23705697d55b7c55b755de0dcf
Mostafa-Thabet2020 <62522809+Mostafa-Thabet2020@users.noreply.github.com>
2022-04-02 23:43:15 +0200

    Generate

--- FolderManager/Program.cs
+++ FolderManager/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using FolderManager.Domain;
+using System.IO;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello for File manager");
@@ -18,7 +19,9 @@ for (; ; )
             Console.WriteLine("Choose action\n" +
                 "1.copy files from folder to another folder\n" +
                 "2.Cut files from folder to another folder\n" +
-                "3.Delete folder\n\n");
+                "3.Delete folder\n" +
+                "4.Report folder\n" +
+                "5.Close\n\n");
             int Action = Convert.ToInt32(Console.ReadLine());
             switch (Action)
             {
@@ -102,7 +105,7 @@ for (; ; )
                             {
                                 if (Directory.Exists(FolderPath))
                                 {
-                                    Directory.Delete(FolderPath,true);
+                                    Directory.Delete(FolderPath, true);
                                     Console.Clear();
                                     Console.WriteLine($"Folder is deleted: {FolderPath}");
                                 }
@@ -115,6 +118,68 @@ for (; ; )
                             }
 
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Beep();
+                        Console.WriteLine($"Exeption: {ex.Message}\nDate of error: {DateTime.Now}\n\n");
+                    }
+                    break;
+                case 4:
+                    try
+                    {
+                        Console.WriteLine("Insert folder path");
+                        string FolderPath = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(FolderPath))
+                        {
+                            if (Directory.Exists(FolderPath))
+                            {
+                                List<FolderReportDto> folderReportDtos = new List<FolderReportDto>();
+                                foreach (string FilePath in Directory.GetFiles(FolderPath))
+                                {
+                                    string FileExtention=Path.GetExtension(FilePath);
+                                    if (folderReportDtos.Any(x=>x.ExtentionType== FileExtention))
+                                    {
+                                        folderReportDtos.Where(x => x.ExtentionType == FileExtention).FirstOrDefault()
+                                            .ExtentionCount += 1;
+                                    }
+                                    else
+                                    {
+                                        FolderReportDto folderReportDto = new FolderReportDto();
+                                        folderReportDto.ExtentionType = FileExtention;
+                                        folderReportDto.ExtentionCount = 1;
+                                        folderReportDtos.Add(folderReportDto);
+                                    }
+                                }
+                                List<string> Lines=new List<string> ();
+                                foreach (FolderReportDto folderReportDto in folderReportDtos)
+                                {
+                                    string line = $"Extention type: {folderReportDto.ExtentionType} - " +
+                                        $"Count: {folderReportDto.ExtentionCount}";
+                                    Lines.Add(line);
+                                }
+                                string NameOfTXT= Path.Combine(FolderPath, $"Report.txt");
+                                File.WriteAllLines(NameOfTXT, Lines);
+                                Console.WriteLine($"Report has been generted on path:\n" +
+                                    $"{NameOfTXT}\n\n");
+
+                                for (int i = 0; i < Lines.Count; i++)
+                                {
+                                    //Console.WriteLine($"{i + 1} {Lines[i]}");
+                                    Console.WriteLine(@"{0} {1}", (i+1), Lines[i]);
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("you inserted wrong path");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please insert folder path");
+                        }
+
                     }
                     catch (Exception ex)
                     {
@@ -221,4 +286,9 @@ for (; ; )
         Console.WriteLine("you insert wrong number please choose again");
     }
 
+}
+public class FolderReportDto
+{
+    public string ExtentionType { get; set; }
+    public int ExtentionCount { get; set; }
 }
